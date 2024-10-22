c9fbd79bdf4f23754a3498387fd0c0061cffb737
Gael Santome Lopez <you@example.comgaelsantome@gmail.com git config --global user.name Gasel>
2024-10-22 15:19:13 +0200

    Ejercicio 15 finalizado

--- src/main/java/com/campusdual/classroom/FreshMerchandise.java
+++ src/main/java/com/campusdual/classroom/FreshMerchandise.java
@@ -4,5 +4,24 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class FreshMerchandise extends Merchandise {
+    private Date expirationDate;
 
+
+    public FreshMerchandise(String name, String uniqueId, String responsibleId, int zone, String area, String shelf, int quantity, Date expirationDate) {
+        super(name, uniqueId, responsibleId, zone, area, shelf, quantity);
+        this.expirationDate = expirationDate;
+    }
+    public Date getExpirationDate(){
+        return this.expirationDate;
+    }
+    public String getSpecificData(){
+        return "Localización " + zone + "\nLa fecha de caducidad es: " +getFormattedDate(expirationDate);
+    }
+    public String getFormattedDate(Date fecha) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        return sdf.format(fecha);
+    }
+    public void printSpecificData(){
+        System.out.println(getSpecificData());
+    }
 }
